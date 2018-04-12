1e62b79c89d740bc9d72d15b83bac794f11cbc4b
Jessica <jessica@Jessicas-MacBook-Pro.local>
2018-04-12 08:03:57 -0700

    first commit

---
+++ epochs.py
@@ -0,0 +1,230 @@
+"""Parsing code for DICOMS and contour files"""
+
+import pydicom
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from PIL import Image, ImageDraw
+import shutil
+
+class create_epoch():
+    """
+    This creates a new epoch and returns the batches
+    """
+    def __init__(self,all_files_dir,batch_size=8):
+        self.all_files_dir = all_files_dir
+        self.file_info_df = create_file_info_df(all_files_dir)
+        self.shuffled_df = self.file_info_df.sample(frac=1).copy()
+        self.batch_size = batch_size
+        self.batch_start = 0
+
+    def get_current_batch(self):
+        if self.batch_start >= self.file_info_df.shape[0]:
+            return None,None
+        else:
+            current_batch = self.shuffled_df.iloc[self.batch_start:np.min((self.batch_start+self.batch_size,self.file_info_df.shape[0]))].copy().reset_index()
+            images = []
+            targets = []
+            for idx, row in current_batch.iterrows():
+                dicom_img = parse_dicom_file(row.dicom_file_name_with_path)
+                img_mask = poly_to_mask(row.data_coords, dicom_img.shape[0], dicom_img.shape[1])
+                images.append(dicom_img)
+                targets.append(img_mask)
+            images = np.array(images)
+            targets = np.array(targets)
+            self.batch_start += self.batch_size
+            return images, targets
+
+    def new_epoch(self):
+        self.shuffled_df = self.file_info_df.sample(frac=1).copy()
+        self.batch_start = 0
+
+
+def create_file_info_df(all_files_dir):
+    """
+    This creates a dataframe containing the contours matched with the patient names.
+    :param all_files_dir: top-level directory containing the contours and dicoms
+    :return file_info_df: dataframe containing the filenames and coordinates of the contours and dicoms
+    """
+    link_file = os.path.join(all_files_dir, 'link.csv')
+    try:
+        link = pd.read_csv(link_file)
+    except:
+        print('Error: link file does not exist')
+        return None
+    if not os.path.isdir(os.path.join(all_files_dir, 'masks')):
+        os.mkdir(os.path.join(all_files_dir, 'masks'))
+
+
+    file_info_df = pd.DataFrame(columns=['image_num',
+                                         'patient_num'
+                                         'original_id',
+                                         'patient_id',
+                                         'contour_file_name_with_path',
+                                         'dicom_file_name_with_path',
+                                         'img_size'
+                                         'data_coords'])
+    for idx, row in link.iterrows():
+        patient_num = int(row.patient_id.split('SCD0000')[1].split('01')[0])
+        # check for errors with the files and reduce the dataset depending on these errors
+        try:
+            contourdir = os.path.join(all_files_dir, 'contourfiles', row.original_id, 'i-contours')
+            contour_files = os.listdir(contourdir)
+            dicomdir = os.path.join(all_files_dir, 'dicoms', row.patient_id)
+            for filename in contour_files:
+                image_nums = []
+                patient_nums = []
+                original_id = []
+                patient_id = []
+                contour_file_names_with_path = []
+                dicom_file_names_with_path = []
+                data_coords = []
+
+                if 'IM-0001' in filename:
+                    try:
+                        image_num = int(filename.split('-')[2])
+                        image_nums.append(image_num)
+
+                        patient_nums.append(patient_num)
+                        original_id.append(row.original_id)
+                        patient_id.append(row.patient_id)
+
+                        contour_file_names_with_path.append(os.path.join(contourdir, filename))
+
+                        dicomfile = os.path.join(dicomdir, str(image_num) + '.dcm')
+                        dicom_file_names_with_path.append(dicomfile)
+
+                        coords = parse_contour_file(os.path.join(contourdir, filename))
+                        data_coords.append(coords)
+                    except:
+                        print('Warning: '+filename+' not read')
+
+                temp_df = pd.DataFrame([image_nums], columns=['image_num'])
+                temp_df['patient_num'] = patient_nums
+                temp_df['original_id'] = original_id
+                temp_df['patient_id'] = patient_id
+                temp_df['contour_file_name_with_path'] = contour_file_names_with_path
+                temp_df['dicom_file_name_with_path'] = dicom_file_names_with_path
+                temp_df['data_coords'] = data_coords
+                file_info_df = pd.concat((file_info_df, temp_df), axis=0)
+        except:
+            print('Error: contour and dicom matching')
+    return file_info_df
+
+
+def parse_contour_file(filename):
+    """Parse the given contour filename
+
+    :param filename: filepath to the contourfile to parse
+    :return: list of tuples holding x, y coordinates of the contour
+    """
+
+    coords_lst = []
+
+    with open(filename, 'r') as infile:
+        for line in infile:
+            coords = line.strip().split()
+
+            x_coord = float(coords[0])
+            y_coord = float(coords[1])
+            coords_lst.append((x_coord, y_coord))
+    return coords_lst
+
+
+def parse_dicom_file(filename):
+    """Parse the given DICOM filename
+
+    :param filename: filepath to the DICOM file to parse
+    :return: array containing DICOM image data
+    """
+
+    try:
+        dcm = pydicom.read_file(filename)
+        dcm_image = dcm.pixel_array
+
+        try:
+            intercept = dcm.RescaleIntercept
+        except AttributeError:
+            intercept = 0.0
+        try:
+            slope = dcm.RescaleSlope
+        except AttributeError:
+            slope = 0.0
+
+        if intercept != 0.0 and slope != 0.0:
+            dcm_image = dcm_image*slope + intercept
+        return dcm_image
+    except:
+        print('Not a valid dicom file')
+        return None
+
+# def parse_contour_file(filename):
+#     """Parse the given contour filename
+#
+#     :param filename: filepath to the contourfile to parse
+#     :return: list of tuples holding x, y coordinates of the contour
+#     """
+#
+#     coords_lst = []
+#
+#     with open(filename, 'r') as infile:
+#         for line in infile:
+#             coords = line.strip().split()
+#
+#             x_coord = float(coords[0])
+#             y_coord = float(coords[1])
+#             coords_lst.append((x_coord, y_coord))
+#
+#     return coords_lst
+
+
+# def parse_dicom_file(filename):
+#     """Parse the given DICOM filename
+#
+#     :param filename: filepath to the DICOM file to parse
+#     :return: dictionary with DICOM image data
+#     """
+#
+#     try:
+#         dcm = dicom.read_file(filename)
+#         dcm_image = dcm.pixel_array
+#
+#         try:
+#             intercept = dcm.RescaleIntercept
+#         except AttributeError:
+#             intercept = 0.0
+#         try:
+#             slope = dcm.RescaleSlope
+#         except AttributeError:
+#             slope = 0.0
+#
+#         if intercept != 0.0 and slope != 0.0:
+#             dcm_image = dcm_image*slope + intercept
+#         dcm_dict = {'pixel_data' : dcm_image}
+#         return dcm_dict
+#     except InvalidDicomError:
+#         return None
+
+
+def poly_to_mask(polygon, width, height):
+    """Convert polygon to mask
+
+    :param polygon: list of pairs of x, y coords [(x1, y1), (x2, y2), ...]
+     in units of pixels
+    :param width: scalar image width
+    :param height: scalar image height
+    :return: Boolean mask of shape (height, width)
+    """
+
+    # http://stackoverflow.com/a/3732128/1410871
+    img = Image.new(mode='L', size=(width, height), color=0)
+    ImageDraw.Draw(img).polygon(xy=polygon, outline=0, fill=1)
+    mask = np.array(img).astype(bool)
+    return mask
+
+if __name__=="__main__":
+    epoch = create_epoch('final_data')
+    images,targets = epoch.get_current_batch()
+    while images is not None:
+        images, targets = epoch.get_current_batch()
---
+++ testing/generate_and_test_outputs.py
@@ -0,0 +1,103 @@
+import sys
+sys.path.insert(0,'../')
+import os
+import shutil
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+from epochs import create_epoch, create_file_info_df, parse_dicom_file, parse_contour_file, poly_to_mask
+
+class check_file_info_outputs():
+    """
+    This checks that the file inputs and outputs are as expected. Currently, this uses visual
+    inspection of the outputs to verify that the contours are what they should be. With more time,
+    a testing framework with setup/teardown would have examples of images with known masks
+    (e.g. a square region) and compare the values created by the mask.
+    """
+    def __init__(self, all_files_dir):
+        self.all_files_dir = all_files_dir
+        self.file_info_df = create_file_info_df(all_files_dir)
+
+    def save_input_and_target_files(self):
+        """
+        This function saves the input and target files in directories with matching image names, in order to check that
+        the inputs and target files are as expected. Once the files are exported, these can be visually inspected.
+        :return: folders containing input and target files
+        """
+        input_dir = os.path.join(self.all_files_dir,'inputs')
+        target_dir = os.path.join(self.all_files_dir,'targets')
+
+        if os.path.isdir(input_dir):
+            shutil.rmtree(input_dir)
+        os.mkdir(input_dir)
+
+        if os.path.isdir(target_dir):
+            shutil.rmtree(target_dir)
+        os.mkdir(target_dir)
+
+        for idx, row in self.file_info_df.iterrows():
+            dicom_img = parse_dicom_file(row.dicom_file_name_with_path)
+            img_mask = poly_to_mask(row.data_coords, dicom_img.shape[0], dicom_img.shape[1])
+            cv2.imwrite(os.path.join(input_dir,'patient'+str(row.patient_num)+'image'+str(row.image_num)+'.png'),dicom_img)
+            cv2.imwrite(os.path.join(target_dir, 'patient' + str(row.patient_num) + 'image' + str(row.image_num) + '.png'),255*img_mask.astype(int))
+
+    def save_images_with_masks(self,annotated_images_dir='test'):
+        """
+        This function saves the input and target images alongside
+        each other in order to verify that they match.
+        :param annotated_images_dir: Directory containing the annotated images
+        :return:images outlined with the contours alongside the masks
+        """
+        saved_images_dir = os.path.join(self.all_files_dir, annotated_images_dir)
+        if os.path.isdir(saved_images_dir):
+            shutil.rmtree(saved_images_dir)
+        os.mkdir(saved_images_dir)
+
+        for idx, row in self.file_info_df.iterrows():
+            dicom_img = parse_dicom_file(row.dicom_file_name_with_path)
+            img_mask = poly_to_mask(row.data_coords, dicom_img.shape[0], dicom_img.shape[1])
+            fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+            ax[0].imshow(dicom_img, cmap='gray')
+            ax[0].plot(np.array(row.data_coords)[:, 0], np.array(row.data_coords)[:, 1], 'r')
+            ax[0].axis('off')
+            ax[1].imshow(img_mask, cmap='gray')
+            ax[1].axis('off')
+            plt.savefig(os.path.join(self.all_files_dir, 'test/patient' + row.patient_id + 'image' + str(row.image_num) + '.png'))
+            plt.close()
+
+    def export_image_info(self,csv_filename='file_info.csv'):
+        self.file_info_df.to_csv(csv_filename)
+
+def check_epochs(filename,out_dir = 'test_epoch_dir'):
+    """
+    This checks that the batches are being generated properly by
+    saving the batches into folders with images and masks. Once the files
+    are exported, these can be visually inspected.
+    :param filename: top-level directory name
+    :param out_dir: output directory containing the image/mask files
+    """
+    if os.path.isdir(out_dir):
+        shutil.rmtree(out_dir)
+    os.mkdir(out_dir)
+    epoch = create_epoch(filename)
+    images, targets = epoch.get_current_batch()
+    batch_num = 0
+    while images is not None:
+        images, targets = epoch.get_current_batch()
+        if images is not None:
+            os.mkdir(os.path.join(out_dir,str(batch_num)))
+            os.mkdir(os.path.join(out_dir, str(batch_num),'images'))
+            os.mkdir(os.path.join(out_dir, str(batch_num),'targets'))
+            for image_num in range(images.shape[0]):
+                cv2.imwrite(os.path.join(out_dir, str(batch_num),'images','image'+str(image_num)+'.jpg'),images[image_num,:,:])
+                cv2.imwrite(os.path.join(out_dir, str(batch_num), 'targets', 'target' + str(image_num) + '.jpg'),
+                            targets[image_num, :, :]*255)
+            batch_num += 1
+
+if __name__=="__main__":
+    C = check_file_info_outputs('../final_data')
+    C.save_images_with_masks()
+    C.save_input_and_target_files()
+    C.export_image_info()
+    check_epochs('../final_data')
